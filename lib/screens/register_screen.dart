33e1c90ebc50c624253d6c9e9ba4368ce0af168f
jhsc <jsantacruz13@gmail.com>
2020-02-11 20:38:31 -0800

    added register screen

---
+++ lib/screens/register_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatefulWidget {
+  RegisterScreen({Key key}) : super(key: key);
+
+  @override
+  _RegisterScreenState createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(
+      child: Column(
+        children: <Widget>[
+          Text("Register here"),
+        ],
+      ),
+    ),
+    );
+  }
+}
