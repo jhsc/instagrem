23da85672fa5c58d9cbb3fb3725a498b5e28bbea
jhsc <jsantacruz13@gmail.com>
2020-02-13 11:05:16 -0800

    Added feed screen

---
+++ lib/screens/feed_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FeedSacreen extends StatefulWidget {
+  FeedSacreen({Key key}) : super(key: key);
+
+  @override
+  _FeedSacreenState createState() => _FeedSacreenState();
+}
+
+class _FeedSacreenState extends State<FeedSacreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
