08078f3390e088992d4a7765c080a0fad1e10ac9
jhsc <jsantacruz13@gmail.com>
2020-02-20 11:39:26 -0800

    Started profile

--- lib/screens/pages/profile_screen.dart
+++ lib/screens/pages/profile_screen.dart
@@ -11,11 +11,91 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Profile'
-        ),
+      backgroundColor: Colors.white,
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 0.0),
+            child: Row(
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: NetworkImage('https://i.redd.it/dmdqlcdpjlwz.jpg'),
+                ),
+                Expanded(
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                        Column(
+                          children: <Widget>[
+                            Text(
+                              '12',
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            Text(
+                              'posts',
+                              style: TextStyle(
+                                color: Colors.black54,
+                              ),
+                            ),
+                          ],
+                        ),
+
+                        Column(
+                          children: <Widget>[
+                            Text(
+                              '386',
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            Text(
+                              'followers',
+                              style: TextStyle(
+                                color: Colors.black54,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: <Widget>[
+                            Text(
+                              '345',
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            Text(
+                              'following',
+                              style: TextStyle(
+                                color: Colors.black54,
+                              ),
+                            ),
+                          ],
+                        ),
+
+                      ],)
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
+
+      // body: Center(
+      //   child: Text(
+      //     'Profile'
+      //   ),
+      // ),
     );
   }
 }
