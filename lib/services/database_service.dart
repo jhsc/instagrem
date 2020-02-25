e279e3329de13834fde8907556c7ae8796c56e91
jhsc <jsantacruz13@gmail.com>
2020-02-24 20:29:14 -0800

    added search query

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:instagrem/models/user.dart';
 import 'package:instagrem/utilities/constants.dart';
 
@@ -8,5 +9,10 @@ class DatabaseService {
       'profileImageUrl': user.profileImageUrl,
       'bio': user.bio,
     });
+  }
+
+  static Future<QuerySnapshot> searchUsers(String name) {
+    Future<QuerySnapshot> users = usersRef.where('name', isGreaterThanOrEqualTo: name).getDocuments();
+    return users;
   }
 }
