26a72a0094db19b997fc4b302729e3a3676dc068
jhsc <jsantacruz13@gmail.com>
2020-02-24 16:30:51 -0800

    Added databse service to talk to db

---
+++ lib/services/database_service.dart
@@ -0,0 +1,12 @@
+import 'package:instagrem/models/user.dart';
+import 'package:instagrem/utilities/constants.dart';
+
+class DatabaseService {
+  static void updateUser(User user) {
+    usersRef.document(user.id).updateData({
+      'name': user.name,
+      'profileImageUrl': user.profileImageUrl,
+      'bio': user.bio,
+    });
+  }
+}
