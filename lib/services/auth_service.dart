3a78b019d955b82292ad79581d3a10208839502b
jhsc <jsantacruz13@gmail.com>
2020-02-24 16:32:01 -0800

    Removed unecessary file

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:instagrem/screens/pages/feed_screen.dart';
 
 class AuthService {
   static final _auth = FirebaseAuth.instance;
