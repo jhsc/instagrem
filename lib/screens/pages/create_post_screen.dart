1228047add30510b8038aba32abdf02f0c6ec3fc
jhsc <jsantacruz13@gmail.com>
2020-03-03 21:25:24 -0800

    started ios implementation

--- lib/screens/pages/create_post_screen.dart
+++ lib/screens/pages/create_post_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CreatePostScreen extends StatefulWidget {
@@ -8,27 +11,79 @@ class CreatePostScreen extends StatefulWidget {
 }
 
 class _CreatePostScreenState extends State<CreatePostScreen> {
+  _showSelectImageDialog() {
+    return Platform.isIOS ? _iosBottomSheet() : _androidDialog();
+  }
+
+  _iosBottomSheet() {
+    showCupertinoDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return CupertinoActionSheet(
+          title: Text('Add Photo'),
+          actions: <Widget>[
+            CupertinoActionSheetAction(
+              child:Text('Take Photo'),
+              onPressed: () => print('Camera'),
+            ),
+            CupertinoActionSheetAction(
+              child:Text('Choose From Gallery'),
+              onPressed: () => print('Gallery'),
+            ),
+          ],
+          cancelButton: CupertinoActionSheetAction(
+            child: Text('Cancel'),
+            onPressed: () => Navigator.pop(context),
+          ),
+        );
+      }
+    );
+  }
+
+  _androidDialog() {
+
+  }
+
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
         title: Center(
           child: Text(
-            'Instagrem',
+            'Create Post',
             style: TextStyle(
               color: Colors.black,
-              fontFamily: 'Billabong',
-              fontSize: 35.0
             ),
           ),
         ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+          )
+        ],
       ),
-      body: Center(
-        child: Text(
-          'Create Post'
-        ),
-      ),
+      body: Column(
+        children: <Widget>[
+          GestureDetector(
+            onTap: _showSelectImageDialog,
+              child: Container(
+              height: width,
+              width: width,
+              color: Colors.grey[300],
+              child: Icon(
+                Icons.add_a_photo,
+                color: Colors.white70,
+                size: 150.0,
+              ),
+            ),
+          )
+        ],
+      )
     );
   }
 }
